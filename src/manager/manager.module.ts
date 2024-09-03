114196819e50211e0b0815ec6ed4bc84dc2ce10b
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-03 14:24:56 +0100

    Building the service of creating an new manager

--- src/manager/manager.module.ts
+++ src/manager/manager.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { ManagerService } from './manager.service';
 import { ManagerController } from './manager.controller';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Manager } from './manager.entity'; // Manager Entity
+
 @Module({
+  imports: [TypeOrmModule.forFeature([ Manager ])],
   providers: [ManagerService],
   controllers: [ManagerController]
 })
--- src/manager/manager.service.ts
+++ src/manager/manager.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Manager } from './manager.entity'; // Manager Entity
 
 @Injectable()
-export class ManagerService {}
+export class ManagerService {
+    constructor(@InjectRepository(Manager) private repo: Repository<Manager>) {}
+
+    create(username: string, password: string, id: number, role: string) {
+        const manager = this.repo.create({ username, password, id, role });
+
+        return this.repo.save(manager);
+    }
+}
