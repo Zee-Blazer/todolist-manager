0dde0a9d60946489bde7009b77d885363e54f1bc
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-03 17:29:29 +0100

    Did not include the validation section for the DTO.... but everything is all good now

--- src/manager/dtos/create-manager.dto.ts
+++ src/manager/dtos/create-manager.dto.ts
@@ -1,11 +1,12 @@
-import { IsString, IsStrongPassword, IsIn } from "class-validator";
+import { IsString, IsStrongPassword, IsNotEmpty, IsIn } from "class-validator";
 
 export class CreateManagerDto {
     @IsString()
+    @IsNotEmpty()
     username: string;
 
     @IsString()
-    @IsStrongPassword()
+    @IsNotEmpty()
     password: string;
 
     @IsIn(["manager"], { message: "The role must be a manager" })
--- src/manager/manager.controller.ts
+++ src/manager/manager.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+
+import { ManagerService } from './manager.service'; // Manager Service
+import { CreateManagerDto } from './dtos/create-manager.dto';
 
 @Controller('manager')
-export class ManagerController {}
+export class ManagerController {
+    constructor (private managerService: ManagerService) {}
+
+    @Post('/auth')
+    createManager(@Body() body: CreateManagerDto) {
+        return this.managerService.create(body.username, body.password, body.role);
+    }
+}
