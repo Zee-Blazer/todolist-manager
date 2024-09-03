2d770aa5a8aeb2406a477da1b987dc3d0589ee3d
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-03 13:46:55 +0100

    Creating the three entities that would be the fundamental block to which this app would run on

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StaffModule } from './staff/staff.module';
+import { ManagerModule } from './manager/manager.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [StaffModule, ManagerModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
