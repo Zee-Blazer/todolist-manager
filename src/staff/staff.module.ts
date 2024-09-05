58e0da07343c2688831462cb21d328708af7e58a
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-05 11:26:03 +0100

    Making all the necessary import for Staff entity

--- src/staff/staff.module.ts
+++ src/staff/staff.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { StaffService } from './staff.service';
 import { StaffController } from './staff.controller';
 
+import { TypeOrmModule } from '@nestjs/typeorm'; // Type ORM Entity
+import { Staff } from './staff.entity'; // Staff entity
+
 @Module({
+  imports: [TypeOrmModule.forFeature([ Staff ])],
   providers: [StaffService],
   controllers: [StaffController]
 })
