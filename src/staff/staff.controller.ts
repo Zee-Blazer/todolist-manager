302afcbc5608e749749636ec18e1632e98550066
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-12 11:43:32 +0100

    Currently working on the staff section

--- src/staff/staff.controller.ts
+++ src/staff/staff.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Patch, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param } from '@nestjs/common';
 
 import { StaffService } from './staff.service'; // Staff entity
 import { CreateStaffDto } from './dtos/create-staff.dto';
@@ -7,6 +7,18 @@ import { CreateStaffDto } from './dtos/create-staff.dto';
 export class StaffController {
     constructor(private staffService: StaffService) {}
 
-    @Post()
-    createStaff(@Body() body: CreateStaffDto) {}
+    @Post("/auth")
+    createStaff(@Body() body: CreateStaffDto) {
+        return this.staffService.create(body.username, body.password, body.role);
+    }
+
+    @Get()
+    allStaffs() {
+        return this.staffService.find();
+    }
+
+    @Get("/:id")
+    specificStaff(@Param('id') id: string) {
+        return this.staffService.findOne(parseInt(id));
+    }
 }
