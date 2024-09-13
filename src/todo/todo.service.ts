a8764eb054901eb236528368491e246e51703751
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-13 08:35:49 +0100

    Adding comment to the todo.service code

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -8,7 +8,7 @@ import { TodoList } from './todo.enity';
 export class TodoService {
     constructor(@InjectRepository(TodoList) private repo: Repository<TodoList>) {}
 
-    // 
+    // This service provides the ability to create personalized todolist
     createPersonalTodo(item: string, ownerId: number) {
         const todolist = this.repo.create({ item, ownerId });
 
